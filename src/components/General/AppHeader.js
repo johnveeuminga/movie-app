882f74a01d3b9cd09fb226b18a88782a146d20d7
John Vee Uminga <johnveeuminga@example.com>
2019-11-14 23:14:11 +0800

    Added AppHeader.

--- src/components/General/AppHeader.js
+++ src/components/General/AppHeader.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '../../assets/movies-logo.png'
 
 function AppHeader() {
   return (
-    <Navbar bg='dark'>
+    <Navbar bg='dark' variant=''>
       <Navbar.Brand
-        href='/'
-      >
-        <img src={logo} alt=""/>
-      </Navbar.Brand>
+        as={({ className }) => {
+          return (
+            <Link to='/' 
+              className={className}
+            >
+              <img src={logo} alt=""/>
+            </Link>
+          )
+        }}
+      />
+      <Nav className='mr-auto'>
+        <Nav.Link as={({ className }) => (
+          <NavLink to='/' className={className} activeClassName={`font-weight-bold`}>
+            Home
+          </NavLink>
+        )}/>
+        <Nav.Link as={({ className }) => (
+          <NavLink to='/movies' className={className} activeClassName={`font-weight-bold`}>
+            Movies
+          </NavLink>
+        )}/>
+        <Nav.Link as={({ className }) => (
+          <NavLink to='/tv-shows' className={className} activeClassName={`font-weight-bold`}>
+            TV Shows
+          </NavLink>
+        )}/>
+      </Nav>
     </Navbar>
   )
 }
